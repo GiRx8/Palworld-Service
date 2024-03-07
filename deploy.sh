6bdbd563c5fce59966bee59c286b76889489ce8f
GiRx8 <70153076+GiRx8@users.noreply.github.com>
2024-03-07 11:36:06 +0100

    deployment correction
    Organize and fixed mistakes in the automatic deployment file.

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
-# Step 1: Install Gunicorn
-echo "Installing Gunicorn..."
-pip install gunicorn
-
-# Step 2: Install Nginx
+# Step 1: Install Nginx
 echo "Installing Nginx..."
 sudo apt update
 sudo apt install -y nginx
 
-# Step 3: Configure Gunicorn
-echo "Configuring Gunicorn..."
-cat <<EOF | sudo tee /etc/systemd/system/gunicorn.service > /dev/null
-
-[Unit]
-Description=Gunicorn instance to serve your Flask application
-After=network.target
-
-[Service]
-User=steam
-Group=steam
-WorkingDirectory=/home/app/pals
-Environment="PATH=/home/app/pals/venv/bin"
-ExecStart=/home/app/pals/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 main:create_app()
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Step 4: Configure Nginx
+# Step 2: Configure Nginx
 echo "Configuring Nginx..."
-cat <<EOF | sudo tee /etc/nginx/sites-available/PalService > /dev/null
+cat <<'EOF' | sudo tee /etc/nginx/sites-available/PalService > /dev/null
 
 server {
     listen 80;
@@ -58,14 +35,17 @@ server {
 }
 EOF
 
+# Step 3: Enable site
 sudo ln -s /etc/nginx/sites-available/PalService /etc/nginx/sites-enabled
 
-# Step 5: Enable and Start Services
+# Step 4: Enable and Start Services
 echo "Enabling and starting services..."
-sudo systemctl enable gunicorn
-sudo systemctl start gunicorn
 sudo systemctl enable nginx
 sudo systemctl start nginx
+
+# Step 5: Install Python venv
+echo "Installing Python 3.10-venv..."
+sudo apt install -y python3.10-venv
 
 # Step 6: Configure Firewall
 echo "Configuring firewall..."
@@ -79,16 +59,51 @@ echo "steam   ALL=(ALL) NOPASSWD: /bin/systemctl start palworld.service, /bin/sy
 # Step 8: Set up Python virtual environment
 echo "Setting up Python virtual environment..."
 cd /home/app/pals
-python3 -m venv venv
+sudo python3 -m venv venv
+sudo chown -R $USER:$USER venv
 source venv/bin/activate
 
-# Step 9: Install Flask and other dependencies
+# Step 9: install gunicorn
+echo "Installing Gunicorn..."
+pip install gunicorn
+
+# Step 10: Configure Gunicorn
+echo "Configuring Gunicorn..."
+cat <<EOF | sudo tee /etc/systemd/system/gunicorn.service > /dev/null
+
+[Unit]
+Description=Gunicorn instance to serve your Flask application
+After=network.target
+
+[Service]
+User=steam
+Group=steam
+WorkingDirectory=/home/app/pals
+Environment="PATH=/home/app/pals/venv/bin"
+ExecStart=/home/app/pals/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 main:create_app()
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 11: Enable and Start Services
+echo "Enabling and starting services..."
+sudo systemctl enable gunicorn
+sudo systemctl start gunicorn
+
+# Step 12: Install Flask and other dependencies
 echo "Installing Flask and dependencies..."
 pip install -r requirements.txt
 
-# Step 10: Deactivate virtual environment
+# Step 13: Deactivate virtual environment
 deactivate
 
+# Step 14: Grant steam user permissions
+echo "Grant steam user folder permission..."
+sudo chown -R steam:steam /home/app
+
+# Step 15: Restart services
+echo "Restarting nginx and gunicorn..."
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
 
